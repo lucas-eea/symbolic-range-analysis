0a72f578832ec015734155cfe4bad329227c89e3
Lucas <lucaseliasalves.a@gmail..com>
2026-04-05 23:47:16 -0300

    Update ESSAfier.cpp

--- lib/ESSAfier.cpp
+++ lib/ESSAfier.cpp
@@ -6,92 +6,170 @@
 #include "llvm/Analysis/DominanceFrontier.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/MapVector.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Utils/SSAUpdater.h"
+#include <map>
 
 using namespace llvm;
 
-bool dominatesUse(Value *V, BasicBlock *Succ, DominatorTree *DT) {
-  for (User *U : V->users()) {
-    if (isa<PHINode>(U)) continue;
-    auto *I = dyn_cast<Instruction>(U);
-    if (!I) continue;
-    if (DT->dominates(Succ, I->getParent())) return true;
-  }
-  return false;
-}
+void insertPHIs(Value* LHS, Value* RHS, BasicBlock* Branch,
+		BasicBlock* BB, StringRef Suffix){
 
-void getDependences(Value* V, BasicBlock* BB,
-                    SmallPtrSet<Instruction*, 16> &Dependences,
-                    SmallPtrSet<Value*, 16> &Visited) {
-  if (!Visited.insert(V).second) return;
 
-  auto *I = dyn_cast<Instruction>(V);
-  if (!I || I->getParent() != BB) return;
+  if (!Branch->getSinglePredecessor()) return;
+  
+  PHINode *SigmaLHS = PHINode::Create(LHS->getType(),
+					1,
+					LHS->getName() + Suffix,
+					Branch->begin());
+  SigmaLHS->addIncoming(LHS, BB);
+  PHINode *SigmaRHS = PHINode::Create(RHS->getType(),
+					1,
+					RHS->getName() + Suffix,
+					Branch->begin());
+  SigmaRHS->addIncoming(RHS, BB);
 
-  for (Value *Op : I->operands()) {
-    auto *OpI = dyn_cast<Instruction>(Op);
-    if (!OpI) continue;
-    Dependences.insert(OpI);
-    getDependences(OpI, BB, Dependences, Visited);
-  }
-}
-
-void insertTransitiveDependences(BasicBlock* BB, BasicBlock* Succ,
-                                 SmallPtrSet<Instruction*, 16> &Dependences,
-                                 DominatorTree &DT,
-                                 StringRef Suffix,
-                                 DenseMap<Value*, Value*> &VMap) {
-  auto InsertPt = Succ->getFirstInsertionPt();
-  for (Instruction &I : *BB) {
-    if (Dependences.count(&I) && dominatesUse(&I, Succ, &DT)) {
-      Instruction *Clone = I.clone();
-      Clone->setName(I.getName() + Suffix);
-      Clone->insertBefore(InsertPt);
-      for (unsigned i = 0; i < Clone->getNumOperands(); i++) {
-        Value *Op = Clone->getOperand(i);
-        if (VMap.count(Op))
-          Clone->setOperand(i, VMap[Op]);
-      }
-      VMap[&I] = Clone;
+  for (Instruction &I : *Branch){
+    if (isa<PHINode>(&I)) continue;
+    for (Use &U : I.operands()){
+      if (U.get() == LHS) U.set(SigmaLHS);
+      if (U.get() == RHS) U.set(SigmaRHS);
     }
   }
 }
 
-void σ(Value* LHS, Value* RHS, BasicBlock* Succ, BasicBlock* BB,
-       DominatorTree &DT, bool isTrueBranch) {
-  bool sameValue = (LHS == RHS);
-  StringRef Suffix = isTrueBranch ? ".t" : ".f";
+void getDominatedDependences(std::map<Value*, Value*> Pred,
+					       std::set<Instruction*> &Copies,
+					       BasicBlock* Branch,
+					       DominatorTree &DT){
+  for (auto &[Key, P] : Pred){
+    Instruction *I = dyn_cast<Instruction>(Key);
+    if (!I) continue;
+    if (any_of(I->users(), [&](User *U){
+      Instruction *UI = dyn_cast<Instruction>(U);
+      return UI && DT.dominates(Branch, UI->getParent());
+    })) {
+      Value *Cur = I;
+      while (Cur){
+	if (Instruction *I = dyn_cast<Instruction>(Cur))
+	  Copies.insert(I);
+	auto It = Pred.find(Cur);
+	Cur = (It != Pred.end()) ? It->second : nullptr;
+      }
+    }
+  }
+}
 
-  PHINode *SigmaLHS = PHINode::Create(LHS->getType(), 1, LHS->getName() + Suffix, Succ->begin());
-  SigmaLHS->addIncoming(LHS, BB);
+void getDependences(Value *V, std::set<Instruction*> &Dependents,
+		    std::map<Value*, Value*> &Pred, BasicBlock *BB){
+  if (!V || isa<Constant>(V)) return;
+  for (User *U : V->users()){
+    Instruction *I = dyn_cast<Instruction>(U);
+    if (!I || isa<PHINode>(I) || isa<ICmpInst>(I)) continue;
+    if (I->getParent() != BB) continue;
+    if (Dependents.count(I)) continue;
+    Dependents.insert(I);
+    Pred[I] = V;
+    getDependences(I, Dependents, Pred, BB);
+  }
+}
 
-  PHINode *SigmaRHS = nullptr;
-  if (!sameValue) {
-    SigmaRHS = PHINode::Create(RHS->getType(), 1, RHS->getName() + Suffix, Succ->begin());
-    SigmaRHS->addIncoming(RHS, BB);
+std::map<Instruction*, Instruction*> copyAndInsert(
+		                     std::set<Instruction*> ToBeCopied,
+		                     BasicBlock* Branch,
+		                     BasicBlock* BB,
+		                     StringRef Suffix){
+  std::map<Instruction*, Instruction*> copies;
+  
+  for (Instruction *I : ToBeCopied){
+    Instruction *copy = I->clone();
+    copy->setName(I->getName() + Suffix);
+    copies[I] = copy;
   }
 
-  DenseMap<Value*, Value*> VMap;
-  VMap[LHS] = SigmaLHS;
-  if (!sameValue) VMap[RHS] = SigmaRHS;
+  BasicBlock::iterator insertPoint = Branch->getFirstNonPHIIt();    
 
-  SmallPtrSet<Value*, 16> VisitedLHS;
-  SmallPtrSet<Instruction*, 16> DependencesLHS;
-  if (auto *I = dyn_cast<Instruction>(LHS))
-    if (I->getParent() == BB)
-      DependencesLHS.insert(I);
-  getDependences(LHS, BB, DependencesLHS, VisitedLHS);
-  insertTransitiveDependences(BB, Succ, DependencesLHS, DT, Suffix, VMap);
+  for (Instruction &I : *BB){
+    if (!ToBeCopied.count(&I)) continue;
 
-  SmallPtrSet<Value*, 16> VisitedRHS;
-  SmallPtrSet<Instruction*, 16> DependencesRHS;
-    if (auto *I = dyn_cast<Instruction>(RHS))
-      if (I->getParent() == BB)
-        DependencesRHS.insert(I);
-  getDependences(RHS, BB, DependencesRHS, VisitedRHS);
-  insertTransitiveDependences(BB, Succ, DependencesRHS, DT, Suffix, VMap);
+    Instruction *copy = copies[&I];
+
+    for (Use &Op : copy->operands()){
+      if (Instruction *OpI = dyn_cast<Instruction>(Op.get()))
+	if (copies.count(OpI))
+	  Op.set(copies[OpI]);
+    }
+    copy->insertBefore(*Branch, insertPoint);
+  }
+
+  for (auto it  = insertPoint; it != Branch->end(); ++it) {
+    for (Use &Op : it->operands()){
+      if (Instruction *OpI = dyn_cast<Instruction>(Op.get()))
+	if (copies.count(OpI))
+	  Op.set(copies[OpI]);
+    }
+  }
+
+  return copies;
 }
+
+void σ(Value* LHS, Value* RHS,
+       BasicBlock* TrueBranch,
+       BasicBlock* FalseBranch,
+       BasicBlock* BB,
+       DominatorTree &DT) {
+
+  std::set<Instruction*> DependentsOnLHS, DependentsOnRHS;
+  std::map<Value*, Value*> Pred;
+  std::set<Instruction*> toBeCopied;
+
+  getDependences(LHS,DependentsOnLHS, Pred, BB);
+  getDependences(RHS, DependentsOnRHS, Pred, BB);
+
+  std::set<Instruction *> toBeCopiedTB, toBeCopiedFB;
+  getDominatedDependences(Pred, toBeCopiedTB, TrueBranch, DT);
+  getDominatedDependences(Pred, toBeCopiedFB, FalseBranch, DT);
+
+  std::map<Instruction*, Instruction*> copiesTB, copiesFB, allCopies;  
+  copiesTB = copyAndInsert(toBeCopiedTB, TrueBranch, BB, ".t");
+  copiesFB = copyAndInsert(toBeCopiedFB, FalseBranch, BB, ".f");
+
+  insertPHIs(LHS, RHS, TrueBranch, BB, ".t");
+  insertPHIs(LHS, RHS, FalseBranch, BB, ".f");
   
+  allCopies = copiesTB;
+  allCopies.insert(copiesFB.begin(), copiesFB.end());
+
+  SSAUpdater Updater;
+  
+  for (auto &[OG, Copy] : allCopies){
+    bool inTB = copiesTB.count(OG);
+    bool inFB = copiesTB.count(OG);
+    Updater.Initialize(OG->getType(), OG->getName());
+    if (inTB && inFB){
+      Updater.AddAvailableValue(TrueBranch, copiesTB[OG]);
+      Updater.AddAvailableValue(FalseBranch, copiesFB[OG]);
+    } else if (inTB){
+      Updater.AddAvailableValue(TrueBranch, copiesTB[OG]);
+      Updater.AddAvailableValue(BB, OG);
+    } else if (inFB){
+      Updater.AddAvailableValue(FalseBranch, copiesFB[OG]);
+      Updater.AddAvailableValue(BB, OG);
+    } else continue;
+    for (Use &U : OG->uses()) {
+      auto *UserInst = cast<Instruction>(U.getUser());
+      BasicBlock *UserBB = UserInst->getParent();
+
+      if (UserBB == BB || UserBB == TrueBranch || UserBB == FalseBranch)
+        continue;
+
+      Updater.RewriteUse(U);
+    }
+  }
+}
+
 struct ESSAfier : public PassInfoMixin<ESSAfier> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM){
     DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
@@ -110,8 +188,7 @@ struct ESSAfier : public PassInfoMixin<ESSAfier> {
       BasicBlock *TrueBranch = BI->getSuccessor(0);
       BasicBlock *FalseBranch = BI->getSuccessor(1);
       
-      if (TrueBranch->getSinglePredecessor()) σ(LHS, RHS, TrueBranch, &BB, DT, true);
-      if (FalseBranch->getSinglePredecessor()) σ(LHS, RHS, FalseBranch, &BB, DT, false);      
+      σ(LHS, RHS, TrueBranch, FalseBranch, &BB, DT);
     }
     return PreservedAnalyses::all();
   }
